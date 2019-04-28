b647408a3a429d441d7ed6ebbcf848fa6777c528
Gabriel Stratan <contact@ithanium.org>
2019-04-28 15:44:01 +0100

    Implementation of fizzbuzz in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::iter::{once, repeat};
+
 fn main() {
-    println!("Hello, world!");
+    let fizzes = repeat("").take(2).chain(once("fizz")).cycle();
+    let buzzes = repeat("").take(4).chain(once("buzz")).cycle();
+    let fizzes_buzzes = fizzes.zip(buzzes);
+
+    let fizz_buzz = (1..100).zip(fizzes_buzzes)
+        .map(|tuple|
+            match tuple {
+                (i, ("", "")) => i.to_string(),
+                (_, (fizz, buzz)) => format!("{}{}", fizz, buzz)
+            });
+    
+    for line in fizz_buzz {
+        println!("{}", line);
+    }
+
+    println!("That was it");
 }
